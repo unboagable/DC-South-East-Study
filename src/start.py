cc3bb4db1835e0fde5bcdbb54765d3c5effd51a3
unboagable <unboagable@gmail.com>
2025-01-13 23:59:45 -0500

    Reorganize files and publish rows

--- src/start.py
+++ src/start.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-FILENAME = 'data/DS4EJ_EJScreen_State_BGLevel_NC_Summarized_By_County.csv'
-
+FILENAME = 'data/EJSCREEN_2024_BG_StatePct_with_AS_CNMI_GU_VI.csv'
 df = pd.read_csv(FILENAME, index_col=0, encoding='utf-8')
 print(df.head(5))
+
+# Step 3: Programmatically Identify Relevant Columns
+def identify_columns(df):
+    print("Available columns:")
+    for col in df.columns:
+        print(col)
+
+if df is not None:
+    identify_columns(df)
